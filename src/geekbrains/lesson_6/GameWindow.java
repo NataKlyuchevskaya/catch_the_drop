09cabc5427875894dccf4c7e39773467269ab91e
Nata <nata.klyuchevskaya@gmail.com>
2022-04-07 20:46:51 +0300

    test2

--- src/geekbrains/lesson_6/GameWindow.java
+++ src/geekbrains/lesson_6/GameWindow.java
@@ -25,7 +25,7 @@ public class GameWindow extends JFrame { //объявляем переменну
         drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
 	    game_window = new GameWindow ();
         game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        game_window.setLocation(200, 100);
+        game_window.setLocation(500, 400);
         game_window.setSize (906, 478);
         game_window.setResizable(false);
         last_frame_time = System.nanoTime();
